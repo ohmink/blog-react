5fab7be98f8ab3ebda4a236bfa58ec01bbd6f9f7
ohmink <kkop5056@naver.com>
2021-03-28 04:49:40 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import React from "react";
+import { Route } from "react-router-dom";
+import { Header } from "./public/header";
+import { MainPage } from "./main/mainPage";
 
 function App() {
-  return <div className="App">My github pages</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Route path="/" exact={true} component={MainPage} />
+    </div>
+  );
 }
 
 export default App;
