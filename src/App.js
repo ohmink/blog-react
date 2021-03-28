43f748c863d325287c6e63695894221a3f6770c4
ohmink <kkop5056@naver.com>
2021-03-29 02:06:10 +0900

    add new page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { Header } from "./public/header";
 import { MainPage } from "./main/mainPage";
+import { ProfilePage } from "./profile/profilePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Route path="/" exact={true} component={MainPage} />
+      <Route path="/profile" component={ProfilePage} />
     </div>
   );
 }
