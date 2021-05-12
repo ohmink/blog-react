80bf3f8f0894910a43c6146e8da2b3d684a5ef62
ohmink <kkop5056@naver.com>
2021-05-12 14:03:48 +0900

    소개글 수정

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import test from "../images/hi_icon.png";
 
 export const Home = () => {
   return (
@@ -15,11 +16,13 @@ export const Home = () => {
       <div className="home_introduce_layout">
         <p>
           안녕하세요!
-          <img width="30" height="30" /> 제 포트폴리오 사이트에 오신 걸
-          환영합니다!
+          <img width="30" height="30" src={test} />
         </p>
+        <p>제 포트폴리오 사이트에 오신 걸 환영합니다!</p>
         <p>이곳에서는 제 프로필, 프로젝트에 대해 살펴볼 수 있으며,</p>
-        <p>/블로그/와 /깃허브/로 이동할 수 있습니다.</p>
+        <p>
+          <b>블로그</b>와 <b> 깃허브</b>로 이동할 수 있습니다.
+        </p>
       </div>
     </div>
   );
