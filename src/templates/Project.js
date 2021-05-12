8e22ecce5f8f75c14c90113fa00bcf3a26dad498
ohmink <kkop5056@naver.com>
2021-05-12 14:03:02 +0900

    프로젝트 소개 화면 디자인

--- src/templates/Project.js
+++ src/templates/Project.js
@@ -1,5 +1,14 @@
 import React from "react";
+import "./Project.css";
+import { ProjectBox } from "./items/ProjectBox";
+import { title, dates, contents } from "./data/ProjectData";
 
 export const Project = () => {
-  return <div>Project 템플릿</div>;
+  return (
+    <div className="project">
+      {title.map((v, i) => (
+        <ProjectBox key={i} title={v} period={dates[i]} content={contents[i]} />
+      ))}
+    </div>
+  );
 };
