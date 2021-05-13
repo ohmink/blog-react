1572373d85b20b58afdab4c970dad303f71d8104
ohmink <kkop5056@naver.com>
2021-05-13 14:27:06 +0900

    헤더 버튼 기능 추가, 디자인 수정

--- src/public/Header.js
+++ src/public/Header.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Header.css";
 
 export const Header = () => {
-  const items = ["Home", "About", "Project", "Blog", "Github"];
-  const Percent = ["", "", ""];
+  const items = ["Home", "About", "Project"];
+  const link = ["Blog", "Github"];
+  const linkTarget = ["https://velog.io/@kasran1", "https://github.com/ohmink"];
+
+  const openLink = (event) => {
+    const id = event.target.id.replace(/[a-zA-Z_]+/g, "");
+    window.open(linkTarget[id]);
+  };
+
+  useEffect(() => {
+    const target = [
+      document.querySelector(".home_template"),
+      document.querySelector(".about"),
+      document.querySelector(".project"),
+    ];
+
+    const buttons = document.querySelectorAll(".header_button");
+    buttons.forEach((button, i) => {
+      button.addEventListener("click", () => target[i].scrollIntoView(true));
+    });
+  }, []);
 
   return (
     <div className="header">
@@ -11,6 +30,17 @@ export const Header = () => {
       <div className="header_button_layout">
         {items.map((item, idx) => (
           <button key={idx} className="header_button">
+            {item}
+          </button>
+        ))}
+        {link.map((item, idx) => (
+          <button
+            key={idx}
+            className="header_link"
+            id={`header_link${idx}`}
+            type="button"
+            onClick={openLink}
+          >
             {item}
           </button>
         ))}
