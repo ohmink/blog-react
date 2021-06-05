972b540e9f03171d5baa3d5336658a196a23eeb4
ohmink <kkop5056@naver.com>
2021-06-05 16:11:22 +0900

    메인 화면 수정

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -12,19 +12,29 @@ export const Home = () => {
             <h2>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Oh-Min</h2>
           </div>
           <div className="book_contents_right">
-            <div></div>
             <div className="book_contents_right_text_container">
-              <h3>궁</h3>
-              <h3>금</h3>
-              <h3>하</h3>
+              <h3>오</h3>
               <h3>신</h3>
-              <h3>가</h3>
-              <h3>요</h3>
-              <h3>?</h3>
+              <br />
+              <h3>것</h3>
+              <h3>을</h3>
+              <br />
+              <h3>환</h3>
+              <h3>영</h3>
+              <h3>합</h3>
+              <h3>니</h3>
+              <h3>다</h3>
+              <h3>.</h3>
             </div>
             <div className="book_contents_right_text_container">
-              <h3>제</h3>
-              <h3>가</h3>
+              <h3>저</h3>
+              <h3>의</h3>
+              <br />
+              <h3>다</h3>
+              <h3>이</h3>
+              <h3>어</h3>
+              <h3>리</h3>
+              <h3>에</h3>
             </div>
           </div>
         </div>
