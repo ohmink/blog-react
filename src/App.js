aac7e89247fe454a44eda8e60862c98086b01046
ohmink <kkop5056@naver.com>
2021-06-07 22:20:38 +0900

    라우터 추가

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { Route } from "react-router";
 import "./App.css";
 import { Header } from "./public/Header";
 import { Home } from "./templates/Home";
+import { About } from "./templates/About";
+import { Project } from "./templates/Project";
+import { Blog } from "./templates/Blog";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className="templates">
-        <Home />
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/project" component={Project} />
+        <Route path="/blog" component={Blog} />
       </div>
     </div>
   );
