297ffdd1071b2a07b73eff89c844bfd7215e73fd
ohmink <kkop5056@naver.com>
2021-06-08 23:01:55 +0900

    태그 로직 수정

--- src/templates/Blog.js
+++ src/templates/Blog.js
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from "react";
 import "./Blog.css";
 import { getAll } from "../utils/BlogApi";
 import { PostsListBox } from "./items/PostsListBox";
+import { Link } from "react-router-dom";
 
 export const Blog = () => {
   const [tags, setTags] = useState(null);
   const [tagCount, setTagCount] = useState(null);
   const [postsData, setPostsData] = useState(null);
   const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     const getPostsData = async () => {
       setLoading(true);
@@ -18,19 +20,21 @@ export const Blog = () => {
       const res = await getAll();
       const tagArray = [];
       const countMap = [];
-
-      res.forEach((r) => {
-        const tags = r.tag.split(" ");
-        tags.forEach((tag) => {
-          if (tagArray.includes(tag)) {
-            countMap[tag]++;
-          } else {
-            tagArray.push(tag);
-            countMap[tag] = 1;
-          }
-        });
-      });
       console.log(res);
+
+      res.forEach((r, i) => {
+        if (r.tag && r.tag.includes(" ")) {
+          const tags = r.tag.split(" ");
+          tags.forEach((tag) => {
+            if (tagArray.includes(tag)) {
+              countMap[tag]++;
+            } else {
+              tagArray.push(tag);
+              countMap[tag] = 1;
+            }
+          });
+        }
+      });
 
       setPostsData(res);
       setTags(tagArray);
@@ -56,15 +60,21 @@ export const Blog = () => {
         </div>
       </div>
       <div className="opened_book_right">
-        {postsData.map((data) => (
-          <PostsListBox
-            key={data._id}
-            title={data.title}
-            tags={data.tag}
-            contents={data.contents}
-            updatedAt={data.updatedAt}
-          />
-        ))}
+        {postsData.map((data) => {
+          if (data.tag && data.contents)
+            return (
+              <PostsListBox
+                key={data._id}
+                title={data.title}
+                tags={data.tag}
+                contents={data.contents}
+                updatedAt={data.updatedAt}
+              />
+            );
+        })}
+      </div>
+      <div className="blog_service_container">
+        <Link to="/new">글쓰기</Link>
       </div>
     </div>
   );
