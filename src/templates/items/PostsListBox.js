6d00f9a9c1a796a8b3b9a9cff75b2a01c84d412e
ohmink <kkop5056@naver.com>
2021-06-09 23:11:36 +0900

    포스트 목록 디자인 수정

--- src/templates/items/PostsListBox.js
+++ src/templates/items/PostsListBox.js
@@ -1,22 +1,23 @@
 import React from "react";
 import "./PostsListBox.css";
 import { TagBox } from "./TagBox";
-import { getPostSummary } from "../../utils/PostSummary";
+import { getPostSummary, getUpdateTime } from "../../utils/PostsHelper";
 
 export const PostsListBox = ({ title, tags, contents, updatedAt }) => {
   const tagArray = tags.split(" ");
   const summary = getPostSummary(contents);
+  const updateTime = getUpdateTime(updatedAt);
 
   return (
     <div className="posts_list_box">
-      <h3>{title}</h3>
+      <h2>{title}</h2>
       <p className="posts_summary">{summary}</p>
       <div className="posts_list_box_tags">
         {tagArray.map((tag, idx) => (
-          <TagBox key={`tag_box_${idx}`} tagName={tag} />
+          <TagBox key={`tag_box_${idx}`} tagName={tag} id={idx} />
         ))}
       </div>
-      <p className="posts_updated_time">{updatedAt}</p>
+      <p className="posts_updated_time">{updateTime}</p>
     </div>
   );
 };
