bd7302a620525bfb72340b59197dda16be7e7617
ohmink <kkop5056@naver.com>
2021-06-10 23:01:48 +0900

    현재 페이지 색상 추가

--- src/public/Header.js
+++ src/public/Header.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
+  const [prePage, setPrePage] = useState(null);
   const items = ["About", "Project", "Blog"];
 
   const openGithub = () => window.open("https://github.com/ohmink");
   const openHome = () => (window.location.href = "/");
+  const movePage = (event) => {
+    const curPage = event.target;
+
+    if (!prePage) {
+      curPage.style.backgroundColor = "lemonchiffon";
+      setPrePage(curPage);
+    } else if (prePage != curPage) {
+      prePage.style.backgroundColor = "rgba(0,0,0,0)";
+      curPage.style.backgroundColor = "lemonchiffon";
+      setPrePage(curPage);
+    }
+  };
 
   return (
     <div className="header">
@@ -18,7 +31,9 @@ export const Header = () => {
           <Link
             key={idx}
             className="header_button"
+            id={`header_button_${item.toLowerCase()}`}
             to={`/${item.toLowerCase()}`}
+            onClick={movePage}
           >
             {item}
           </Link>
