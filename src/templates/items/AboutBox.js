4cbb9c3064c7eda244495db58c52778d79f2a721
ohmink <kkop5056@naver.com>
2021-06-17 21:48:57 +0900

    about 페이지 추가 구현

--- src/templates/items/AboutBox.js
+++ src/templates/items/AboutBox.js
@@ -1,5 +1,34 @@
 import React from "react";
+import styled, { css } from "styled-components";
 
-export const AboutBox = ({ name }) => {
-  return <div className="about_item_box">{name}</div>;
+const BoxLayout = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin: 10px 10px 10px 0;
+  border-radius: 10px;
+  border-width: 3px;
+  border-style: solid;
+  height: 120px;
+  width: 120px;
+  ${(props) =>
+    props.primary &&
+    css`
+      border-color: ${props.borderColor};
+    `};
+`;
+
+const LogoImg = styled.img`
+  width: 100px;
+
+  margin: 10px;
+`;
+
+export const AboutBox = ({ url, color }) => {
+  return (
+    <BoxLayout primary={true} borderColor={color}>
+      <LogoImg src={url} />
+    </BoxLayout>
+  );
 };
