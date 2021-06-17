e01c7d536b9ac7d3c23274f986034a664c7d513a
ohmink <kkop5056@naver.com>
2021-06-17 21:49:15 +0900

    검사 이미지 url 추가

--- src/utils/ImageProvider.js
+++ src/utils/ImageProvider.js
@@ -22,6 +22,26 @@ const urlProvider = {
     "https://user-images.githubusercontent.com/45379812/122220168-09ae8200-ceeb-11eb-940f-d0ee522add0e.png",
     "https://user-images.githubusercontent.com/45379812/122220172-0adfaf00-ceeb-11eb-9c2e-398b03750ef1.png",
   ],
+  skills: [
+    "https://user-images.githubusercontent.com/45379812/122303062-ea8d1000-cf3d-11eb-8b2f-ff00ef248215.png",
+    "https://user-images.githubusercontent.com/45379812/122303072-ec56d380-cf3d-11eb-975e-85151b64d34e.png",
+    "https://user-images.githubusercontent.com/45379812/122303070-ebbe3d00-cf3d-11eb-9ae0-34a0c7eafec7.png",
+    "https://user-images.githubusercontent.com/45379812/122303051-e8c34c80-cf3d-11eb-99f4-790a32c6b873.png",
+    "https://user-images.githubusercontent.com/45379812/122303065-eb25a680-cf3d-11eb-8ff4-d191bc2332b8.png",
+    "https://user-images.githubusercontent.com/45379812/122303058-e95be300-cf3d-11eb-93c3-8ec7b7068a36.png",
+    "https://user-images.githubusercontent.com/45379812/122303068-ebbe3d00-cf3d-11eb-8cf6-bcf049b94b4c.png",
+  ],
+  tools: [
+    "https://user-images.githubusercontent.com/45379812/122303064-eb25a680-cf3d-11eb-9ae0-7861015ec338.png",
+    "https://user-images.githubusercontent.com/45379812/122303061-ea8d1000-cf3d-11eb-8e9b-44f6a671b31c.png",
+  ],
+  test: {
+    personality:
+      "https://user-images.githubusercontent.com/45379812/122382577-037cdc00-cfa5-11eb-8961-da16533becaa.png",
+    aptitute:
+      "https://user-images.githubusercontent.com/45379812/122382571-01b31880-cfa5-11eb-8e76-850468537139.png",
+    mind: "https://user-images.githubusercontent.com/45379812/122382573-02e44580-cfa5-11eb-9ee9-3bdbbc955dab.png",
+  },
 };
 
 export default urlProvider;
