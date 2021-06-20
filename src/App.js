cce7886edb546fad6f308a012ff086c59f219cb9
ohmink <kkop5056@naver.com>
2021-06-21 01:19:46 +0900

    경로 수정

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Header } from "./public/Header";
 import { About } from "./templates/About";
 import { Project } from "./templates/Project";
 import { Blog } from "./templates/Blog";
-import { NewBlog } from "./templates/NewBlog";
+import { NewPosts } from "./templates/NewPosts";
 import { Posts } from "./templates/Posts";
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
         <Route path="/post/:postsId" exact component={Posts} />
         <Route path="/about" component={About} />
         <Route path="/project" component={Project} />
-        <Route path="/blog/new" component={NewBlog} />
+        <Route path="/new" component={NewPosts} />
       </div>
     </div>
   );
