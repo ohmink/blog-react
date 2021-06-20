15f43ac91d8677d2b2dc8c81cd98aeeca3072260
ohmink <kkop5056@naver.com>
2021-06-21 01:17:20 +0900

    로그인 화면 구현

---
+++ src/templates/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Login.css";
+import { login } from "../utils/PostsApi";
+
+export const Login = ({ history }) => {
+  console.log(history);
+  const closeLogin = () => {
+    const loginTemplate = document.querySelector(".login_template");
+    loginTemplate.style.display = "none";
+  };
+
+  const tryLogin = async () => {
+    try {
+      const username = document.getElementById("input_username");
+      const password = document.getElementById("input_password");
+
+      const status = await login({
+        username: username.value,
+        password: password.value,
+      });
+
+      if (status >= 200 && status < 300) {
+        history.push("/new");
+      }
+    } catch (error) {
+      return error;
+    }
+  };
+
+  return (
+    <div className="login_template">
+      <div>
+        <p className="login_close_button" onClick={closeLogin}>
+          X
+        </p>
+      </div>
+      <p>Admin ID</p>
+      <input type="text" id="input_username" />
+      <p>Admin PW</p>
+      <input type="password" id="input_password" />
+      <button onClick={tryLogin}>Login</button>
+    </div>
+  );
+};
