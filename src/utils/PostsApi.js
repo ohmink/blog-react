0f4a6348c13bc74323416908548c89709c1cb658
ohmink <kkop5056@naver.com>
2021-06-21 01:18:05 +0900

    서버 주소 변경

--- src/utils/PostsApi.js
+++ src/utils/PostsApi.js
@@ -1,20 +1,36 @@
 import axios from "axios";
 
+export const login = async (data) => {
+  const response = await axios.post(
+    "https://ohmink-blog-server.herokuapp.com/user/login",
+    data,
+    {
+      withCreadentials: true,
+    }
+  );
+
+  axios.defaults.headers.common[
+    "Authorization"
+  ] = `Bearer ${response.data.access_token}`;
+
+  return response.status;
+};
+
 export const getAll = async () => {
   try {
-    const answer = await axios.get(`http://localhost:3001/posts/getAll`);
+    const answer = await axios.get(
+      `https://ohmink-blog-server.herokuapp.com/posts/getAll`
+    );
     return answer.data;
   } catch (error) {
     return error;
   }
 };
-
-const getTagList = () => {};
 
 export const getDetail = async (postsId) => {
   try {
     const answer = await axios.get(
-      `http://localhost:3001/posts/getDetail/${postsId}`
+      `https://ohmink-blog-server.herokuapp.com/posts/getDetail/${postsId}`
     );
     return answer.data;
   } catch (error) {
@@ -22,12 +38,13 @@ export const getDetail = async (postsId) => {
   }
 };
 
-export const create = async (posts) => {
+export const create = async (data) => {
   try {
     const response = await axios.post(
       "http://localhost:3001/posts/create",
-      posts
+      data
     );
+
     return response;
   } catch (error) {
     return error;
