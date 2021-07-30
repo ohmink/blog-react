13b5b51d39985a46e221093c12ce469e08b7124c
ohmink <kkop5056@naver.com>
2021-07-30 16:28:05 +0900

    태그 색상 랜덤하게 변경

--- src/pages/items/TagBox.js
+++ src/pages/items/TagBox.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./styles/TagBox.css";
 
-export const TagBox = ({ tagName, id }) => {
-  const colors = ["#616e9a", "#aa85b3", "#f9b5ae"];
+export const TagBox = ({ tagName }) => {
+  const colors = [
+    "#616e9a",
+    "#aa85b3",
+    "#f9b5ae",
+    "#81d4cf",
+    "#fc651e",
+    "#8e32ac",
+    "#5849be",
+    "#d73642",
+    "#2bc3d1",
+  ];
+  const randomIdx = Math.floor(Math.random() * 8);
 
   return (
-    <span style={{ backgroundColor: colors[id] }} className="tag_box">
+    <span style={{ backgroundColor: colors[randomIdx] }} className="tag_box">
       {tagName}
     </span>
   );
