7350c5e36d69359e641ed30e501766dbc2e4072a
ohmink <kkop5056@naver.com>
2021-12-07 22:35:31 +0900

    시분초 삭제

--- src/utils/PostsHelper.js
+++ src/utils/PostsHelper.js
@@ -15,14 +15,6 @@ export const getUpdateTime = (updateTime) => {
   let day = time.getDate();
   if (day < 10) day = `0${day}`;
 
-  // let hour = time.getHours();
-  // if (hour < 12) hour = `오전 ${hour}`;
-  // else if (hour === 12) hour = `오후 ${hour}`;
-  // else hour = `오후 ${hour - 12}`;
-
-  // let minute = time.getMinutes();
-  // if (minute < 10) minute = `0${minute}`;
-
   return `${year}-${month}-${day}`;
 };
 
